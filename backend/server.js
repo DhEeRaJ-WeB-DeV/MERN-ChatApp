ed97d1224d8aba595407b3dfa495de0cd20b73e5
dheeraj <dheerajsure595@gmail.com>
2024-11-20 19:08:43 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authroutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 
 const port=process.env.PORT||1080;
 
+const __dirname=path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
@@ -21,6 +24,11 @@ app.use("/api/auth",authroutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port,()=>{
         connectToMangoDB();
