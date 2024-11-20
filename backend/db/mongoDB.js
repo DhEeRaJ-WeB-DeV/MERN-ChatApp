127b862961fcbb9d6257f9e836b6bf003870934c
dheeraj <dheerajsure595@gmail.com>
2024-11-20 20:34:17 +0530

    mongo-DB

--- backend/db/mongoDB.js
+++ backend/db/mongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMangoDB=async()=>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/");
+        await mongoose.connect("mongodb+srv://dheerajsure595:L9l02sP6OG50vvoy@cluster0.ju9lj.mongodb.net/chat-app-db?retryWrites=true&w=majority&appName=Cluster0");
         console.log("connected to mangodb");
     }
     catch(error){
