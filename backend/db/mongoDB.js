0368bb7097ab2d4453e93b63a09a6d587fc9625a
DhEeRajwed <dheerajsure595@gmail.com>
2024-11-20 22:00:52 +0530

    Update mongoDB.js

--- backend/db/mongoDB.js
+++ backend/db/mongoDB.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const connectToMangoDB=async()=>{
     try{
         await mongoose.connect(process.env.MONGO_DB_URI);
-        console.log("connected to mangodb");
+        console.log("connected to mangodb", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 50000, // Timeout after 50 seconds
+          });
     }
     catch(error){
          console.log("error connecting to mangodb",error.message);
